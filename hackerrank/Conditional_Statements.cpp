ce82aac61dd973f65e5ff44e106fded60afef343
davidalong800 <davidalong800@gmail.com>
2026-09-17 18:34:44 +0300

    HackerRank: Conditional Statements

---
+++ hackerrank/Conditional_Statements.cpp
@@ -0,0 +1,52 @@
+// HackerRank — Conditional Statements
+// https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    // Write your code here
+    if (1 <= n && n <= 9) {
+        string words[] = {"one", "two", "three", "four", "five",
+                          "six", "seven", "eight", "nine"};
+        cout << words[n - 1] << "\n";
+    } else {
+        cout << "Greater than 9\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
